dea869e3f2a1b673e4db68234f803ecf11bf70f1
Iyamu Joshua <ayevbeosa.j@gmail.com>
2018-12-21 23:12:55 +0100

    Set TextView from Intent

--- jokedisplay/src/main/java/com/udacity/gradle/jokedisplay/JokeActivity.java
+++ jokedisplay/src/main/java/com/udacity/gradle/jokedisplay/JokeActivity.java
@@ -2,12 +2,23 @@ package com.udacity.gradle.jokedisplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
+
+    public static final String EXTRA_JOKE = "com.udacity.gradle.jokedisplay.extras.EXTRA_JOKE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+        if (getIntent() != null) {
+            TextView jokeText = findViewById(R.id.joke_text);
+            String joke = getIntent().getStringExtra(EXTRA_JOKE);
+            if (!TextUtils.isEmpty(joke)) {
+                jokeText.setText(joke);
+            }
+        }
     }
 }
