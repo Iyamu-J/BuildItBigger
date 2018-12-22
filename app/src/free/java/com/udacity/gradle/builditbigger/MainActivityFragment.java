6b8472a3e27e2516cd537a69fcfa60a3aa1ab360
Iyamu Joshua <ayevbeosa.j@gmail.com>
2018-12-22 22:48:24 +0100

    Add free product flavour

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,20 +1,23 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.jokedisplay.JokeActivity;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements JokeRetriever {
 
     public MainActivityFragment() {
     }
@@ -25,6 +28,7 @@ public class MainActivityFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
         AdView mAdView = root.findViewById(R.id.adView);
+        Button jokeButton = root.findViewById(R.id.joke_btn);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
@@ -32,6 +36,28 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
+        jokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getJoke();
+            }
+        });
         return root;
+    }
+
+    private void getJoke() {
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(this);
+        endpointsAsyncTask.execute();
+
+    }
+
+    @Override
+    public void onRetrieveJoke(String jokeResult) {
+        if (jokeResult != null) {
+            Intent intent = new Intent(getActivity(), JokeActivity.class);
+            intent.putExtra(JokeActivity.EXTRA_JOKE, jokeResult);
+            startActivity(intent);
+        }
     }
 }
