c98290eeac36b98964eaa20a0d55e1bf8193db2f
Iyamu Joshua <ayevbeosa.j@gmail.com>
2018-12-23 17:36:24 +0100

    Return null to prevent a false positive value

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -1,6 +1,7 @@
 package com.udacity.gradle.builditbigger;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
@@ -42,7 +43,8 @@ public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
         try {
             return myApiService.getJoke().execute().getData();
         } catch (IOException e) {
-            return e.getMessage();
+            Log.i("Error Retrieving Jokes:", e.getMessage());
+            return null;
         }
     }
 
