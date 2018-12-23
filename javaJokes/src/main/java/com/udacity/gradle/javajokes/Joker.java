45bc4c5e2e0c254eb85b207fa2b7dfb0c833d0cf
Iyamu Joshua <ayevbeosa.j@gmail.com>
2018-12-23 17:23:06 +0100

    Add more jokes

--- javaJokes/src/main/java/com/udacity/gradle/javajokes/Joker.java
+++ javaJokes/src/main/java/com/udacity/gradle/javajokes/Joker.java
@@ -1,8 +1,19 @@
 package com.udacity.gradle.javajokes;
 
+import java.util.Random;
+
 public class Joker {
 
+    private String[] jokesArray = new String[]{
+            "How do functions break up? \"They stop calling each other!\"",
+            "In what unit do you measure a function's length? \"In Para meters...\"",
+            "What did the flirtatious function say? \"Call me ;)\"",
+            "Q: How do you tell an introverted computer scientist from an extroverted computer scientist?\n" +
+                    "\n" +
+                    "A: An extroverted computer scientist looks at your shoes when he talks to you."
+    };
+
     public String getJokes() {
-        return "Funny Joke from JavaJokes library";
+        return jokesArray[new Random().nextInt(jokesArray.length)];
     }
 }
